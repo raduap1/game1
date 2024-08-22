a675c374754d3189f8889cb31638de39ae9c2b13
Radu <radu.podea@yahoo.com>
2024-08-22 06:16:08 -0700

    Initial commit

---
+++ Assets/Scripts/BounceScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class BounceScript : MonoBehaviour
+{
+    private float startX;
+    private float startY;
+
+    private Rigidbody2D rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        startX = rb.position.x;
+        startY = rb.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.AddForce(transform.up * 500f);
+        }
+
+    }
+
+    void OnCollisionEnter2D()
+    {
+        //rb.MovePosition(d);
+        rb.AddForce(transform.up * 600f);
+    }
+}
